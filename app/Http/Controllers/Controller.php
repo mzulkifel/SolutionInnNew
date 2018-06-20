c016576fc59ff24962daebc74417a0134f15230e
mzulkifel <zulkifel@outlook.com>
2018-06-20 10:52:44 +0500

    New Push with Laravel Files and React JS: Additional Component Push 2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,47 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'title' => $request->get('title'),
+            'body' => $request->get('body')
+        ]);
+        $product->save();
+
+
+        return response()->json('Product Added Successfully.');
+    }
+
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->title = $request->get('title');
+        $product->body = $request->get('body');
+        $product->save();
+
+
+        return response()->json('Product Updated Successfully.');
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product Deleted Successfully.');
+    }
 }
